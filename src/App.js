33f7ccb558040dded4e66f0515ea30315374458a
esipple2 <esipple2@huskers.unl.edu>
2024-10-15 11:56:04 -0500

    Added countdown timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Countdown from './components/countdown';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <h1>Cornhacks 2025</h1>
+      <Countdown />
     </div>
   );
 }
---
+++ src/components/countdown.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import "./countdown.css";
+
+const Countdown = () => {
+    const targetDate = new Date('2025-02-17T00:00:00');
+    const [timeLeft, setTimeLeft] = useState(timeRemaining());
+
+    function timeRemaining() {
+        const date = new Date();
+        const difference = targetDate - date;
+
+        let time = {
+            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+            minutes: Math.floor((difference / (1000 * 60)) % 60),
+            seconds: Math.floor((difference / 1000) % 60),
+        };
+        
+        return difference > 0 ? time : { days: 0, hours: 0, minutes: 0, seconds: 0 };
+    }
+
+    useEffect(() => {
+        const timer = setInterval(() => setTimeLeft(timeRemaining()), 1000);
+        return () => clearInterval(timer);
+      }, []);
+    
+      return (
+        <div className="container">
+          <div className="timer">
+            <div className="time-box">
+              <p className="time">{timeLeft.days}</p>
+              <span>Days</span>
+            </div>
+            <div className="time-box">
+              <p className="time">{timeLeft.hours}</p>
+              <span>Hours</span>
+            </div>
+            <div className="time-box">
+              <p className="time">{timeLeft.minutes}</p>
+              <span>Minutes</span>
+            </div>
+            <div className="time-box">
+              <p className="time">{timeLeft.seconds}</p>
+              <span>Seconds</span>
+            </div>
+          </div>
+        </div>
+      );
+}
+
+export default Countdown;
