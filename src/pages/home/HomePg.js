936ecd5610d8284bead66a70012c7e1063142018
esipple2 <esipple2@huskers.unl.edu>
2024-10-20 00:25:11 -0500

    Added image wheel

--- src/pages/home/HomePg.js
+++ src/pages/home/HomePg.js
@@ -3,15 +3,25 @@ import Countdown from '../../components/countdown'
 import NavBar from '../../features/NavBar'
 import croppedLogo from '../../Images/cropped_raw.png'
 import './HomePg.css'
+import ImageGallery from '../../features/ImageGallery'
+
 const HomePg = () => {
   return (
-    <><div>
-      <NavBar />
-      <Countdown />
-    </div><div className="floating-image-container">
+    <>
+      <div className="page-container">
+        <NavBar />
+        <Countdown />
+      </div>
+      
+      <div className="floating-image-container">
         <img src={croppedLogo} alt="Logo" className='img' />
         <span id="shadow"></span>
-      </div></>
+      </div>
+
+      <div className="gallery-container">
+        <ImageGallery />
+      </div>
+      </>
   )
 }
 
