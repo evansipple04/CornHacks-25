c54372b5debe7ec44f0f3b56b9426851b43982e9
ghenderson10 <ghenderson10@huskers.unl.edu>
2024-10-19 20:36:49 -0500

    Added all final pages for the site for now and started on the sponsor page

---
+++ src/pages/register/RegisterPg.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RegisterPg = () => {
+  return (
+    <div>RegisterPg</div>
+  )
+}
+
+export default RegisterPg
--- src/pages/sponsor/Sponsors.js
+++ src/pages/sponsor/Sponsors.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import NavBar from '../../features/NavBar';
+import nelnet from '../../Images/nelnet.png'
+
 
 const Sponsor = () => {
   return (
-    <div>Sponsors</div>
+    <div>
+      <NavBar/>
+      <div>
+        <header>
+          <label>
+            Our Sponsors
+          </label>
+          <div>
+            <img src={nelnet} alt='nelnet' />
+          </div>
+        </header>
+      </div>
+    </div>
   )
 }
 
