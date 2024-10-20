abd05a06e7c48f1c7364d344ff40bb107567b05e
ghenderson10 <ghenderson10@huskers.unl.edu>
2024-10-19 23:59:53 -0500

    Added the basic layout to the sponsor's page will need a lot of work and changed the css file from module.css to just .css

--- src/pages/sponsor/Sponsors.js
+++ src/pages/sponsor/Sponsors.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import NavBar from '../../features/NavBar';
-import nelnet from '../../Images/nelnet.png'
+import nelnet from '../../Images/nelnet.png';
+import olson from '../../Images/olsson.png'
+import './spawn.css';
 
 
 const Sponsor = () => {
   return (
-    <div>
+    <section>
       <NavBar/>
-      <div>
-        <header>
-          <label>
+      <div className='title'>
+        <label>
             Our Sponsors
-          </label>
-          <div>
-            <img src={nelnet} alt='nelnet' />
-          </div>
-        </header>
+        </label>
+        <div className='Plat'>
+          <label>Platnium Sponsors 2025</label>
+          <img className='platImg' src={nelnet} alt='nelnet'/>
+        </div>
+        <div className='Gold'>
+          <label>Gold Sponsors 2025</label>
+          <img className='goldImg' src={olson} alt='olson'/>
+        </div>
+        <div className='Silver'>
+          <label>Silver Spon</label>
+        </div>
       </div>
-    </div>
+    </section>
   )
 }
 
