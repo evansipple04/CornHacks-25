ad1eb8a082e3710bf4f0da976d3aa9278ecee391
jonatag12 <jguzman12@huskers.unl.edu>
2024-10-23 13:55:06 -0500

    added the buttons to get to the past projects

--- src/pages/past_proj/PastProjPg.js
+++ src/pages/past_proj/PastProjPg.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import NavBar from '../../features/NavBar';
+import './PastProjPg.css';
 
 const PastProjPg = () => {
   return (
-    <div>PastProjPg</div>
+    <body>
+      <NavBar/>
+      <div className='title'>
+        <h1>
+          Past Projects
+        </h1>
+        <button className='proj2024'>
+          <label>2024 - Beat The Odds</label>
+        </button>
+        <button className='proj2023'>
+        <label>2023 - CyberSecurity</label>
+        </button>
+        <button className='proj2022'>
+        <label>2022 - Retro</label>
+        </button>
+        <button className='proj2021'>
+        <label>2021 - Looking Forward</label>
+        </button>
+        <button className='proj2020'>
+        <label>2020 - Going Green</label>
+        </button>
+        <button className='proj2019'>
+        <label>2019 - CornHacks 1</label>
+        </button>
+      </div>
+    </body>
   )
 }
 
