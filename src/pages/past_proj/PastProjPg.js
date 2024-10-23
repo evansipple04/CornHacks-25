6fc1941dbb339feea84b871492f42a1dc30d9b14
jonatag12 <jguzman12@huskers.unl.edu>
2024-10-23 17:10:36 -0500

    made the way the page looks be more similar to the old one.

--- src/pages/past_proj/PastProjPg.js
+++ src/pages/past_proj/PastProjPg.js
@@ -10,23 +10,25 @@ const PastProjPg = () => {
         <h1>
           Past Projects
         </h1>
-        <button className='proj2024'>
-          <label>2024 - Beat The Odds</label>
+      </div>
+      <div className='btns'>
+        <button className='proj2024' type="button">
+          <label>2024: Beat The Odds</label>
         </button>
-        <button className='proj2023'>
-        <label>2023 - CyberSecurity</label>
+        <button className='proj2023' type="button">
+          <label>2023: CyberSecurity</label>
         </button>
-        <button className='proj2022'>
-        <label>2022 - Retro</label>
+        <button className='proj2022' type="button">
+          <label>2022: Retro</label>
         </button>
-        <button className='proj2021'>
-        <label>2021 - Looking Forward</label>
+        <button className='proj2021' type="button">
+          <label>2021: Looking Forward</label>
         </button>
-        <button className='proj2020'>
-        <label>2020 - Going Green</label>
+        <button className='proj2020' type="button">
+          <label>2020: Going Green</label>
         </button>
-        <button className='proj2019'>
-        <label>2019 - CornHacks 1</label>
+        <button className='proj2019' type="button">
+          <label>2019: CornHacks 1</label>
         </button>
       </div>
     </body>
