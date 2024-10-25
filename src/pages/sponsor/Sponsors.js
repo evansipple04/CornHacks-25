87feb1d64f8d4938682056565822fbaf2cd7aa2f
ghenderson10 <ghenderson10@huskers.unl.edu>
2024-10-24 19:04:12 -0500

    Very basic sponsors page and set up with examples very much under construction

--- src/pages/sponsor/Sponsors.js
+++ src/pages/sponsor/Sponsors.js
@@ -1,31 +1,63 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import NavBar from '../../features/NavBar';
 import nelnet from '../../Images/nelnet.png';
 import olson from '../../Images/olsson.png'
 import './spawn.css';
-
+import gsap from 'gsap';
+import { Link } from 'react-router-dom';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 const Sponsor = () => {
+  const platy = useRef();
+  const scroller = useRef();
+  const ref = useRef();
+
+  useEffect(() => {    
+    let hoes = gsap.utils.toArray('.spawnArea');
+    hoes.forEach(hoe => {
+      gsap.fromTo(hoe,{x: -600, opacity: 0}, {
+        opacity: 1,
+        x: 0,
+        ease: 'none',
+        duration: 2,
+        scrollTrigger: {
+          trigger: hoe,
+          start:'center 70%',
+          toggleActions: "play none none reset",
+        },
+      })
+    })
+});
+
   return (
-    <section>
+    <>
       <NavBar/>
       <div className='title'>
         <label>
-            Our Sponsors
+            Our 2025 Sponsors
         </label>
-        <div className='Plat'>
-          <label>Platnium Sponsors 2025</label>
-          <img className='platImg' src={nelnet} alt='nelnet'/>
-        </div>
-        <div className='Gold'>
-          <label>Gold Sponsors 2025</label>
-          <img className='goldImg' src={olson} alt='olson'/>
-        </div>
-        <div className='Silver'>
-          <label>Silver Spon</label>
-        </div>
       </div>
-    </section>
+      <div className='divSpace'/>
+      <div className='sponsor-container' ref={scroller}>
+        <div className='spawnArea' ref={platy}>
+          <label className='title colored-box platinum'>Our 2025 Platinum Sponsors</label>
+          <div className='Plat'>
+            <Link to={"https://www.olsson.com/"}>
+              <img src={olson} alt='olson' className='imagePlatinum' />
+            </Link>
+          </div>
+          </div>
+        <div className='spawnArea' ref={ref}>
+          <label className='title colored-box gold'>Our 2025 Gold Sponsors</label>
+          <div className='Gold'>
+            <Link to={"https://www.nelnet.com/"}>
+              <img src={nelnet} alt='nelnet' className='imageGold' />
+            </Link>
+          </div>
+        </div>
+        </div>
+    </>
   )
 }
 
