404d2a6ed9d129cd4e69ee996035050bdf71273f
ghenderson10 <ghenderson10@huskers.unl.edu>
2024-10-26 17:33:24 -0500

    added onto the sponsosr and cleaned the read me for future use

--- src/pages/sponsor/Sponsors.js
+++ src/pages/sponsor/Sponsors.js
@@ -1,36 +1,38 @@
 import React, { useRef, useEffect } from 'react'
 import NavBar from '../../features/NavBar';
 import nelnet from '../../Images/nelnet.png';
-import olson from '../../Images/olsson.png'
+import olson from '../../Images/olsson.png';
+import sandhills from '../../Images/sandhills.png'
 import './spawn.css';
+import { useGSAP } from '@gsap/react'
 import gsap from 'gsap';
 import { Link } from 'react-router-dom';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 const Sponsor = () => {
-  const platy = useRef();
   const scroller = useRef();
-  const ref = useRef();
-
   useEffect(() => {    
     let areas = gsap.utils.toArray('.spawnArea');
     areas.forEach(area => {
       gsap.fromTo(area,{x: -600, opacity: 0}, {
         opacity: 1,
         x: 0,
-        ease: 'none',
+        ease: "power3",
         scrollTrigger: {
           trigger: area,
-          start:'center 70%',
-          toggleActions: "play none none reverse",
+          start:'center 50%',
+          end: '+=100px',
+          //pin: true,
+          scrub: 3,
+          toggleActions: "play none none",
         },
       })
     })
-});
+}, {scope: '.sponsor-container'});
 
   return (
-    <>
+    <section className='mainHold'>
       <NavBar/>
       <div className='title'>
         <label>
@@ -38,25 +40,40 @@ const Sponsor = () => {
         </label>
       </div>
       <div className='divSpace'/>
-      <div className='sponsor-container' ref={scroller}>
-        <div className='spawnArea' ref={platy}>
-          <label className='title colored-box platinum'>Our 2025 Platinum Sponsors</label>
-          <div className='Plat'>
-            <Link to={"https://www.olsson.com/"}>
-              <img src={olson} alt='olson' className='imagePlatinum' />
-            </Link>
+      <section className='sponsor-container' ref={scroller}>
+        <div className='spawnArea'>
+          <div className='title colored-box platinum'>
+            <label>Our 2025 Platinum Sponsors</label>
+            <div className='Plat'>
+              <Link to={"https://www.olsson.com/"}>
+                <img src={olson} alt='olson' className='imagePlatinum' />
+              </Link>
           </div>
+            </div>
           </div>
-        <div className='spawnArea' ref={ref}>
-          <label className='title colored-box gold'>Our 2025 Gold Sponsors</label>
+        <div className='spawnArea'>
+          <div className='title colored-box gold'>
+          <label>Our 2025 Gold Sponsors</label>
           <div className='Gold'>
             <Link to={"https://www.nelnet.com/"}>
               <img src={nelnet} alt='nelnet' className='imageGold' />
             </Link>
           </div>
+          </div>
         </div>
+        <div className='spawnArea'>
+          <div className='title colored-box silver'>
+            <label>Our 2025 Gold Sponsors</label>
+            <div className='Silver'>
+              <Link to={"https://www.sandhills.com/"}>
+                <img src={sandhills} alt='SandHills' className='imageGold' />
+              </Link>
+            </div>
+          </div>
         </div>
-    </>
+        <div className='divSpace'/>
+        </section>
+    </section>
   )
 }
 
