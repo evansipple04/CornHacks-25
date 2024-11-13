0e69f62d4c45d4f3217bf33ceae7183c780a9523
olwenng <79112630+olwenng@users.noreply.github.com>
2024-11-13 01:39:30 -0600

    Open Help Organizer link to the new tab

--- src/features/NavBar.js
+++ src/features/NavBar.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './NavBar.module.css';
-import './NavBar.css'
-import croppedLogo from '../Images/2025Logo.webp';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.module.css";
+import "./NavBar.css";
+import croppedLogo from "../Images/2025Logo.webp";
 
 const NavBar = () => {
   return (
     <div>
       <nav>
         <Link to="/">
-        <img src={croppedLogo} alt="Logo" className='logo'/>
+          <img src={croppedLogo} alt="Logo" className="logo" />
         </Link>
         <ul>
           <li>
             <Link to="/">
-            <button className="Home">Home</button>
+              <button className="Home">Home</button>
             </Link>
           </li>
           <li>
@@ -24,18 +24,22 @@ const NavBar = () => {
           </li>
           <li>
             <Link to="/pastProjects">
-               <button className="PastProjects">Past Projects</button>
+              <button className="PastProjects">Past Projects</button>
             </Link>
           </li>
           <li>
-            <Link to="https://docs.google.com/forms/d/e/1FAIpQLSfKXfQ-5Ppt3dTxJjdI4FfmHuOhnEx1qB4oLgkKqYK_mLal0Q/viewform?embedded=true">
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSfKXfQ-5Ppt3dTxJjdI4FfmHuOhnEx1qB4oLgkKqYK_mLal0Q/viewform?embedded=true"
+              target="_blank"
+              rel="noreferrer"
+            >
               <button className="HelpOrganize">Help Organize</button>
-            </Link>
+            </a>{" "}
           </li>
         </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
